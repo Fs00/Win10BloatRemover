d1392446c71287df721e8f1d2c47d85f265f0ccf
Fs00 <francescosaltori@gmail.com>
2019-07-01 15:04:26 +0200

    Change license to BSD 3-Clause Clear and include it into the program

---
+++ src/Operations/LicensePrinter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Resources;
+using Win10BloatRemover.Utils;
+
+namespace Win10BloatRemover.Operations
+{
+    class LicensePrinter : IOperation
+    {
+        public void PerformTask()
+        {
+            var resources = new ResourceManager("Win10BloatRemover.resources.Resources", typeof(Program).Assembly);
+            ConsoleUtils.WriteLine(resources.GetString("LicenseText"), ConsoleColor.Cyan);
+        }
+    }
+}

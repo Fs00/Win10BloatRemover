Fs00/Win10BloatRemover

commit bcf5dadd0ac38417a628117f89c0e0b0b0a660a3

Delete temporary state repository DB copy when replacement fails

src/Operations/SystemAppRemovalEnabler.cs
@@ -56,7 +56,7 @@ namespace Win10BloatRemover.Operations
         private string CopyStateRepositoryDatabase()
         {
             var stateRepositoryDatabase = new FileInfo(STATE_REPOSITORY_DB_PATH);
-            FileInfo copiedDatabase = stateRepositoryDatabase.CopyTo(STATE_REPOSITORY_DB_NAME, overwrite: true);
+            FileInfo copiedDatabase = stateRepositoryDatabase.CopyTo($"{STATE_REPOSITORY_DB_NAME}.tmp", overwrite: true);
             return copiedDatabase.FullName;
         }
 
@@ -113,7 +113,14 @@ namespace Win10BloatRemover.Operations
 
         private void ReplaceStateRepositoryDatabaseWith(string databaseCopyPath)
         {
-            File.Move(databaseCopyPath, STATE_REPOSITORY_DB_PATH, overwrite: true);
+            try
+            {
+                File.Copy(databaseCopyPath, STATE_REPOSITORY_DB_PATH, overwrite: true);
+            }
+            finally
+            {
+                File.Delete(databaseCopyPath);
+            }
         }
     }
 }

commit b2e1023c8946aa4d0a3bcd817b7048fcfe1f2687

Fix regression introduced by commit bcf5dad

src/Operations/SystemAppRemovalEnabler.cs
@@ -113,13 +113,16 @@ namespace Win10BloatRemover.Operations
 
         private void ReplaceStateRepositoryDatabaseWith(string databaseCopyPath)
         {
+            // File.Copy can't be used to replace the file because it fails with access denied
+            // even though we have Restore privilege, so we need to use File.Move instead
             try
             {
-                File.Copy(databaseCopyPath, STATE_REPOSITORY_DB_PATH, overwrite: true);
+                File.Move(databaseCopyPath, STATE_REPOSITORY_DB_PATH, overwrite: true);
             }
-            finally
+            catch
             {
                 File.Delete(databaseCopyPath);
+                throw;
             }
         }
     }
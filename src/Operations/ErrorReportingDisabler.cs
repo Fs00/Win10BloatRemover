5e031450813088783f4c4303c62576e27015d00d
Fs00 <francescosaltori@gmail.com>
2020-06-11 18:21:11 +0200

    Slightly improve and refactor ErrorReportingDisabler

--- src/Operations/ErrorReportingDisabler.cs
+++ src/Operations/ErrorReportingDisabler.cs
@@ -22,10 +22,9 @@ namespace Win10BloatRemover.Operations
         private void DisableErrorReporting()
         {
             ui.PrintHeading("Writing values into the Registry...");
-            using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows\Windows Error Reporting"))
-                key.SetValue("Disabled", 1, RegistryValueKind.DWord);
-            using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Microsoft\Windows\Windows Error Reporting"))
-                key.SetValue("Disabled", 1, RegistryValueKind.DWord);
+            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Error Reporting", "Disabled", 1);
+            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\PCHealth\ErrorReporting", "DoReport", 0);
+            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\Windows Error Reporting", "Disabled", 1);
         }
 
         private void RemoveErrorReportingServices()

569eb7052f5c5d9676eea36cf81e93545bff473f
Fs00 <francescosaltori@gmail.com>
2020-07-25 21:55:34 +0200

    Bump version to 4.0
    We're almost there!

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -11,16 +11,8 @@ using Win10BloatRemover;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili
-// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da
-// COM, impostare su true l'attributo ComVisible per tale tipo.
 [assembly: ComVisible(false)]
-
-// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
 [assembly: Guid("759f045d-adbf-4867-a0f7-f2d066e0c390")]
 
-// È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
-// usando l'asterisco '*' come illustrato di seguito:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2." + Program.SUPPORTED_WINDOWS_RELEASE_ID)]
-[assembly: AssemblyFileVersion("3.2." + Program.SUPPORTED_WINDOWS_RELEASE_ID)]
+[assembly: AssemblyVersion("4.0." + Program.SUPPORTED_WINDOWS_RELEASE_ID)]
+[assembly: AssemblyFileVersion("4.0." + Program.SUPPORTED_WINDOWS_RELEASE_ID)]

3ab23e275a4ac0edf70a33d524ae3afb47808d84
Fs00 <francescosaltori@gmail.com>
2020-11-28 21:11:41 +0100

    Fix services backup folder name using 12hr clock

--- src/Operations/ServiceRemover.cs
+++ src/Operations/ServiceRemover.cs
@@ -25,7 +25,7 @@ namespace Win10BloatRemover.Operations
         public ServiceRemover(IUserInterface ui, DateTime now)
         {
             this.ui = ui;
-            backupDirectory = new DirectoryInfo($"servicesBackup_{now:yyyy-MM-dd_hh-mm-ss}");
+            backupDirectory = new DirectoryInfo($"servicesBackup_{now:yyyy-MM-dd_HH-mm-ss}");
         }
 
         public ServiceRemover(string[] servicesToRemove, IUserInterface ui, DateTime now) : this(ui, now)

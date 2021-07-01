09298c5686987f669e0d0ac2212d9b98b8c9443c
Fs00 <francescosaltori@gmail.com>
2021-07-01 21:15:20 +0200

    Truly fix Spotlight disabling on Home and Pro editions

--- src/Operations/SuggestionsDisabler.cs
+++ src/Operations/SuggestionsDisabler.cs
@@ -28,6 +28,12 @@ namespace Win10BloatRemover.Operations
             // Personalization -> Start -> Show suggestions occasionally in Start
             RegistryUtils.SetForCurrentAndDefaultUser(
                 @"Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "SubscribedContent-338388Enabled", 0);
+            // Removes fun facts for Spotlight images from lock screen
+            RegistryUtils.SetForCurrentAndDefaultUser(
+                @"Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "SubscribedContent-338387Enabled", 0);
+            // Personalization -> Lock screen -> Get fun facts, tips, tricks and more on your lock screen
+            RegistryUtils.SetForCurrentAndDefaultUser(
+                @"Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "RotatingLockScreenOverlayEnabled", 0);
             // Privacy -> General -> Show suggested content in Settings app
             RegistryUtils.SetForCurrentAndDefaultUser(
                 @"Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "SubscribedContent-338393Enabled", 0);
@@ -37,12 +43,13 @@ namespace Win10BloatRemover.Operations
                 @"Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "SubscribedContent-353696Enabled", 0);
             // System -> Notifications & actions -> Suggest ways I can finish setting up my device...
             RegistryUtils.SetForCurrentAndDefaultUser(
-                @"Software\Microsoft\Windows\CurrentVersion\UserProfileEngagement",
-                "ScoobeSystemSettingEnabled", 0);
+                @"Software\Microsoft\Windows\CurrentVersion\UserProfileEngagement", "ScoobeSystemSettingEnabled", 0);
+
+            // Applies only to Education and Enterprise editions
+            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\CloudContent", "DisableSoftLanding", 1);
 
             Registry.SetValue(
-                @"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsInkWorkspace",
-                "AllowSuggestedAppsInWindowsInkWorkspace", 0);
+                @"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsInkWorkspace", "AllowSuggestedAppsInWindowsInkWorkspace", 0);
             // Disables online tips and help for Settings app
             Registry.SetValue(@"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer", "AllowOnlineTips", 0);
         }
@@ -51,16 +58,16 @@ namespace Win10BloatRemover.Operations
         {
             ui.PrintHeading("Disabling Spotlight, News and Interests and other cloud content via Registry edits...");
 
-            // These two policies work only on Education and Enterprise editions
-            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\CloudContent", "DisableSoftLanding", 1);
+            // These two policies apply only to Education and Enterprise editions
             Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\CloudContent", "DisableWindowsConsumerFeatures", 1);
-
             RegistryUtils.SetForCurrentAndDefaultUser(
-                @"Software\Policies\Microsoft\Windows\CloudContent",
-                "DisableWindowsSpotlightFeatures", 1);
+                @"Software\Policies\Microsoft\Windows\CloudContent", "DisableWindowsSpotlightFeatures", 1);
+            // Applies only to Pro, Education and Enterprise editions
             RegistryUtils.SetForCurrentAndDefaultUser(
-                @"Software\Policies\Microsoft\Windows\CloudContent",
-                "DisableTailoredExperiencesWithDiagnosticData", 1);
+                @"Software\Policies\Microsoft\Windows\CloudContent", "DisableTailoredExperiencesWithDiagnosticData", 1);
+            // This is needed to disable Spotlight on Windows 10 Home and Pro
+            RegistryUtils.SetForCurrentAndDefaultUser(
+                @"Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "RotatingLockScreenEnabled", 0);
 
             // News and Interests
             Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds", "EnableFeeds", 0);
